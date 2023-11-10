8cb6d1f506d6fc69827b599aa742ef73106bfd2c
Lucile THIEULIN <lucile.thieulin96@gmail.com>
2023-11-10 15:45:16 +0100

    changement image en webp

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -10,18 +10,23 @@ import Error from './routes/Error';
 import SignIn from "./routes/SignIn"
 import Profile from './routes/profile';
 import SignUp from './routes/signUp';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 //redux
 import { Provider } from 'react-redux';
 import {configureStore} from '@reduxjs/toolkit'
 import rootReducer from "./reducers"
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
 
 
 const store = configureStore ({
   reducer : rootReducer, 
   devTools: true, // penser à passer en devtool false une fois le site finis pour que le store ne soit plus visible
 })
+
+
+
 
 
 
