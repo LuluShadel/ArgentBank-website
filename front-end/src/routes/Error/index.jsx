2a64a0d8f4748c6327cf11ae9d33130e12e0f542
Lucile THIEULIN <lucile.thieulin96@gmail.com>
2023-11-15 18:15:25 +0100

    ajout de la page error

--- front-end/src/routes/Error/index.jsx
+++ front-end/src/routes/Error/index.jsx
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
+import Prise from "../../assets/fils_electrique.avif"
+import "../../style/error.css"
 
 
 export default function Error () {
     return(
-        <div>
-            error
+        <div className="error">
+            <h1 className="error_title">404 ERROR</h1>
+            <p className="error_text">Sorry, the page not found</p>
+            <img src={Prise} alt="fils débranchés" />
+            <Link className="error_link" to="/"> Return to the HomePage </Link>
         </div>
     )
 }
